d55bec0e84a744e296b052e6f1ca927836828452
hxdegawa <hxdegawa@gmail.com>
2019-12-08 23:42:14 +0900

    Implement OGP & Favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,45 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      {
+        name: 'twitter:image',
+        content: 'https://xn--y8j1e4aj.com/ogp.png'
+      },
+      {
+        name: 'twitter:description',
+        content: 'みらくる.com - ゆめかわみらくるはっしんちゅう'
+      },
+      { name: 'twitter:title', content: 'みらくる.com' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'みらくる.com - ゆめかわみらくるはっしんちゅう'
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'みらくる.com'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://xn--y8j1e4aj.com/'
+      },
+      { hid: 'og:title', property: 'og:title', content: 'みらくる.com' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'みらくる.com - ゆめかわみらくるはっしんちゅう'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://xn--y8j1e4aj.com/ogp.png'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
   /*
    ** Customize the progress-bar color
